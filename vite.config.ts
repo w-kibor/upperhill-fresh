f8f8faadee6f38c9f77af0a8338d05daabf6afd1
Wilkister Kibor <kiborwilkister29@gmail.com>
2026-04-01 23:07:50 +0300

    fetching from url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@
   import react from '@vitejs/plugin-react-swc';
   import tailwindcss from '@tailwindcss/vite';
   import path from 'path';
+  import { fileURLToPath } from 'url';
+
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
   export default defineConfig({
     plugins: [react(), tailwindcss()],
